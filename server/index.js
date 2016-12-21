d24191c88a3e86c9293c390c4920c356a0a518a2
Chris McNeese <christophermcneese@mac.com>
2016-12-21 15:28:31 -0600

    Setup Vue Router

--- server/index.js
+++ server/index.js
@@ -18,3 +18,26 @@ module.exports = function (app) {
     next();
   });
 };
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import App from './routes/application.vue';
+// import Index from './routes/index.vue';
+
+Vue.use(VueRouter);
+
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'index',
+//     component: Index,
+//   },
+// ];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
+const app = new Vue({ ...App, router }).$mount('.app');
